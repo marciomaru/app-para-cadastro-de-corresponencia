ea8b1cc1f2fadcd9ebd2cdbdc9badfbccb29549c
marcio <marciosousar@hotmail.com>
2020-09-12 12:26:13 -0300

    adicionado metodos para verificação nos campos de cadastro

--- tela_de_cadastro_de_itens.py
+++ tela_de_cadastro_de_itens.py
@@ -1,6 +1,7 @@
 from bd import Bd
 from item import Item
 from cadastrar_item import Cadastrar_item
+from tkinter import messagebox
 from tkinter import *
 from tkinter.ttk import Combobox
 
@@ -9,42 +10,71 @@ class Tela_de_cadastro_de_itens:
 
     def __init__(self, root, itens_cbox):
 
-        self.jan = Toplevel()
+        self.__jan = Toplevel()
         self.__root = root
         self.__itens_cbox = itens_cbox
 
-        self.l_nome = Label(self.jan, text='Nome ')
-        self.l_nome.grid(row=0, column=0)
+        self.__l_nome = Label(self.__jan, text='Nome ')
+        self.__l_nome.grid(row=0, column=0)
 
-        self.campo = Entry(self.jan, width=75)
-        self.campo.grid(row=0, column=1)
+        self.__campo = Entry(self.__jan, width=75)
+        self.__campo.grid(row=0, column=1)
 
-        self.l_cj = Label(self.jan, text='Cj ')
-        self.l_cj.grid(row=0, column=2)
+        self.__l_cj = Label(self.__jan, text='Cj ')
+        self.__l_cj.grid(row=0, column=2)
 
-        self.cbox = Combobox(self.jan)
-        self.cbox['values'] = self.__itens_cbox
-        self.cbox.current(0)
-        self.cbox.grid(row=0, column=3)
+        self.__cbox = Combobox(self.__jan)
+        self.__cbox['values'] = self.__itens_cbox
+        self.__cbox.current(0)
+        self.__cbox.grid(row=0, column=3)
 
-        self.l_desc = Label(self.jan, text='Descrição ')
-        self.l_desc.grid(row=1, column=0)
+        self.__l_desc = Label(self.__jan, text='Descrição ')
+        self.__l_desc.grid(row=1, column=0)
 
-        self.campo_desc = Entry(self.jan, width=75)
-        self.campo_desc.grid(row=1, column=1)
+        self.__campo_desc = Entry(self.__jan, width=75)
+        self.__campo_desc.grid(row=1, column=1)
 
-        self.botao_cadastrar = Button(self.jan, text='Cadastrar', command=self.__cadastrar_item)
-        self.botao_cadastrar.grid(row=2, column=0, columnspan=3, pady=10 )
+        self.__botao_cadastrar = Button(self.__jan, text='Cadastrar', command=self.__cadastrar_item)
+        self.__botao_cadastrar.grid(row=2, column=0, columnspan=3, pady=10 )
 
-        self.botao_limpar = Button(self.jan, text='Limpar')
-        self.botao_limpar.grid(row=2, column=1, columnspan=3, pady=10)
+        self.__botao_limpar = Button(self.__jan, text='Limpar')
+        self.__botao_limpar.grid(row=2, column=1, columnspan=3, pady=10)
 
-        self.jan.geometry('750x200')
-        self.jan.transient(self.__root)
-        self.jan.focus_force()
-        self.jan.grab_set()
+        self.__jan.geometry('750x200')
+        self.__jan.transient(self.__root)
+        self.__jan.focus_force()
+        self.__jan.grab_set()
+
+    def __verificar_campos_preenchidos(self):
+        if self.__campo.get() != '' and (self.__campo_desc.get() != ''):
+            return self.__verificar_valor_no_cbox()
+        else:
+            messagebox.showinfo(
+                'ERRO AO CADASTRAR ITEM',
+                'Preencha os campos "Nome" e "Descrição"'
+            )
+
+    def __verificar_valor_no_cbox(self):
+        try:
+            valor_cbox = int(self.__cbox.get())
+            if valor_cbox in self.__itens_cbox:
+                return True
+            else:
+                messagebox.showinfo(
+                    'ERRO AO CADASTRAR ITEM',
+                    'Valor no campo "cj" está incorreto'
+                )
+                return False
+        except:
+            messagebox.showinfo(
+                'ERRO AO CADASTRAR ITEM',
+                'Valor no campo "cj" está incorreto'
+            )
+            return False
 
     def __cadastrar_item(self):
-        item = Item(None, self.campo.get(), self.cbox.get(), self.campo_desc.get())
-        obj_para_cadastar_item = Cadastrar_item(item, Bd())
-        obj_para_cadastar_item.cadastrar_item_no_bd()
+        if self.__verificar_campos_preenchidos():
+            item = Item(None, self.__campo.get(), self.__cbox.get(), self.__campo_desc.get())
+            obj_para_cadastar_item = Cadastrar_item(item, Bd())
+            obj_para_cadastar_item.cadastrar_item_no_bd()
+
